7f15d9a03305afbd3c0f17302a8dd7065fda4905
Miguel Martínez <miguel.martinez.fue@gmail.com>
2024-12-03 20:07:42 -0300

    portafolio actualizado, falta js repositorios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // const githubUsername = "TU_USUARIO_DE_GITHUB";
-const githubUsername = "MiguelMartinez27";
+const githubUsername = "Miguelmartinez27";
 const projectContainer = document.getElementById("project-cards");
 
 // Función para truncar texto
